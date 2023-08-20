e4e3cddebe1f6b385b4a6a67800f3c10c599fef0
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-08-20 12:58:16 -0500

    updated incorrect paths to index.html

--- routes/html/index.js
+++ routes/html/index.js
@@ -4,11 +4,11 @@ const router = require('express').Router();
 const path = require('node:path');
 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 router.get('/notes', (req, res ) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 module.exports = router;

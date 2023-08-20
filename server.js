fd4e18118c52bb22589b1143a512b055ee69dfd8
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-08-20 12:08:49 -0500

    udpated relative path to api and html routes

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use(require('./routes'));
+app.use('./api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);

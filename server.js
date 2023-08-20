7cf7265c12036a6765545c9d695e4f6100983fd1
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-08-20 18:15:08 -0500

    nonfunctional changee: added comments for clarity (i am confused)

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
+// DEPENDENCIES
 const express = require('express');
-
-// returns an object containing the user environment
-const PORT = process.env.PORT || 8080;
-// creates a new express app
-const app = express();
-// const routes require('./routes');
 const apiRoutes = require('./routes/api');
 const htmlRoutes = require('./routes/html');
 
+// CREATE EXPRESS SERVER
+const app = express();
+
+// PORT
+const PORT = process.env.PORT || 8080;
+
+// USE STATIC FILES AND JSON
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
+// ROUTERS
 app.use('./api', apiRoutes);
 app.use('/', htmlRoutes);
 
+// LISTENER
 app.listen(PORT, () => {
     console.log(`app listening on PORT ${PORT}`);
     console.log('press ctrl+c to quit.');
